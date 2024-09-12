3180b59d3e03359c603592d3cc05f0e3e79f9a6f
kanakoAzm <knka0717@gmail.com>
2024-09-12 10:57:25 +0900

    SQL課題Q3を回答

--- db_lesson.sql
+++ db_lesson.sql
@@ -9,3 +9,39 @@ CREATE TABLE departments (
 
 ALTER TABLE people
 ADD COLUMN department_id INT UNSIGNED AFTER email;
+
+-- Q3
+
+INSERT INTO departments (name)
+  VALUES
+  ('営業'),
+  ('開発'),
+  ('経理'),
+  ('人事'),
+  ('情報システム');
+
+INSERT INTO people (name, email, department_id, age, gender)
+  VALUES
+  ('遠藤とおる', 'endo@gizumo.jp', 1, 35, 1),
+  ('江本かおり', 'emoto@gizumo.jp', 1, 30, 2),
+  ('榎本そうた', 'enomoto@gizumo.jp', 1, 24, 1),
+  ('加藤あやね', 'kato@gizumo.jp', 2, 26, 2),
+  ('狩野えいと', 'kano@gizumo.jp', 2, 29, 1),
+  ('上田ことこ', 'kamita@gizumo.jp', 2, 24, 2),
+  ('神田もとき', 'kanda@gizumo.jp', 2, 32, 1),
+  ('毛谷あい', 'ketani@gizumo.jp', 3, 28, 2),
+  ('神保こうた', 'jinbo@gizumo.jp', 4, 25, 1),
+  ('城さくらこ', 'jo@gizumo.jp', 5, 28, 2);
+
+INSERT INTO reports (person_id, content)
+  VALUES
+  (7, 'お疲れ様です。営業レッスン1が終わりました。'),
+  (8, 'お疲れ様です。営業レッスン2が終わりました。'),
+  (9, 'お疲れ様です。営業レッスン3が終わりました。'),
+  (10, 'お疲れ様です。開発レッスン1が終わりました。'),
+  (11, 'お疲れ様です。開発レッスン2が終わりました。'),
+  (12, 'お疲れ様です。開発レッスン3が終わりました。'),
+  (13, 'お疲れ様です。開発レッスン4が終わりました。'),
+  (14, 'お疲れ様です。経理レッスン１が終わりました。'),
+  (15, 'お疲れ様です。人事レッスン1が終わりました。'),
+  (16, 'お疲れ様です。情報システムレッスン1が終わりました。');
